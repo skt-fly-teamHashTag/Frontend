0d4a15773ea29afdeb275b2b1bd65d3530944f56
BBIYAC <kiy7833@gmail.com>
2023-01-02 21:39:39 +0900

    feat(Login): 로그인 기능 구현

---
+++ screens/Login.js
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet, 
+  TextInput, 
+  View,
+  ImageBackground,
+  StatusBar,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+
+import axios from 'axios';
+
+const CustomButton = (props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.8} style={styles.button} onPress={props.onPress}>
+      <Text style={styles.btnText}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const LoginModal = () => {
+  const [user, setUser] = useState({
+    nickName: '',
+    phoneNumber: ''
+  });
+
+  const onPress = async() => {
+    try { // 로그인 정보 POST
+      const response = await axios.post('http://localhost:8080/api/v1/auth/login', user);
+      if (response.data === "로그인 성공") console.log("POST >>", user);
+    } catch(error) {
+      console.log("Error >>", error)
+    }
+  };
+
+  return (
+    <View style={styles.loginModal}>
+      <Text style={styles.textTitle}>닉네임</Text>
+      <TextInput 
+        style={styles.textInput}
+        onChangeText={(text) => setUser({...user, nickName: text})}></TextInput>
+      <Text style={styles.textTitle}>전화번호</Text>
+      <TextInput 
+        onChangeText={(text) => setUser({...user, phoneNumber: text})}
+        style={styles.textInput}></TextInput>
+      <CustomButton title='로그인' onPress={onPress}></CustomButton>
+    </View>
+  );
+};
+
+const Login = () => {
+  return (
+    <ImageBackground 
+      source={require("../images/splash.webp")} 
+      style={styles.bgImage}>
+      <StatusBar barStyle="dark-content"></StatusBar>
+      <LoginModal></LoginModal>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  bgImage: {
+    width: '100%', 
+    height: '100%',
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  loginModal: {
+    width: '80%',
+    backgroundColor: '#F4F6F9',
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    ...Platform.select({
+      ios: {
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 5,
+          height: 5,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+  },
+  textTitle: {
+    alignSelf: 'flex-start',
+    margin: 5,
+    fontSize: 16
+  },
+  textInput: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    margin: 5,
+    marginBottom: 10,
+    padding: 10
+  },
+  button: {
+    width: '100%',
+    height: 50,
+    backgroundColor: "#384BF5",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    margin: 10
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+});
+
+export default Login;
