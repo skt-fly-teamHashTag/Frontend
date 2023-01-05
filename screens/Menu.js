2b6330b5ce235a54e7e02a3273580146016d62d9
BBIYAC <kiy7833@gmail.com>
2023-01-05 17:35:01 +0900

    feat(Menu): 상단 아이콘 버튼 기능

--- screens/Menu.js
+++ screens/Menu.js
@@ -5,18 +5,32 @@ import {
   Text,
   TouchableOpacity
 } from 'react-native';
+import { Icon } from '@rneui/themed';
 
 const Menu = (props) => {
   return (
     <View style={styles.menu}>
-      <TouchableOpacity onPress={() => props.navigation.closeDrawer()}>
-        <Text>메뉴 화면</Text>
-      </TouchableOpacity>
+      <View style={styles.headerRight}>
+        <Icon name="setting" size={24} style={styles.headerIcon} type='ant-design' />
+        <TouchableOpacity onPress={() => props.navigation.closeDrawer()}>
+        <Icon name="close" size={24} style={styles.headerIcon} type='ant-design' />
+        </TouchableOpacity>
+      </View>
+      <Text>메뉴 화면</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  headerRight: {
+    position: 'absolute',
+    right: 0,
+    top: 40,
+    flexDirection: 'row'
+  },
+  headerIcon:{
+    marginRight: 20
+  },
   menu: {
     width: '100%',
     height: '100%',
