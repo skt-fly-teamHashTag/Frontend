3bac1dca0872bed352b7b8e3fd8cc400053bd263
BBIYAC <kiy7833@gmail.com>
2023-01-09 11:51:24 +0900

    refactor(Menu): 안쓰는 코드 제거

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { View,} from 'react-native';
 import MenuHeader from '../components/Header/MenuHeader';
 import MenuScrollView from '../components/ScrollView/MenuScrollView';
-import { setNavigation } from '../slices/menuSlice';
 
 const Menu = ({ navigation }) => {
   return (
