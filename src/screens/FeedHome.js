719af54bdebafd41509ff2dd2e8eb435e7e62ffb
BBIYAC <kiy7833@gmail.com>
2023-01-13 23:06:35 +0900

    chore(FeedHome): 인기 급상승 영상

--- src/screens/FeedHome.js
+++ src/screens/FeedHome.js
@@ -1,41 +1,121 @@
 import React from "react";
 import axios from "axios";
-import { StyleSheet, View, Text, ScrollView } from "react-native";
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { Icon } from "@rneui/themed";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Video from "react-native-video";
 
 const FeedHome = () => {
+  const videoUri = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4";
+  const loadingUri = "/Users/in-yeong/iykim/videoDot/src/assets/videoLoadingGif.gif";
+  const hashTags = '#V #목적없이떠나는여행 #꼬막비빔밥';
+
   return (
     <View style={styles.container}>
-      <Text>나만의 Vlog</Text>
-      <Text>이번주 Hot 랭킹을 확인해보세요!!</Text>
-      <ScrollView
-        horizontal={true}
-        showsHorizontalScrollIndicator = {false}
-        pagingEnabled
-        style={styles.horizontalScrollView}>
-        <View style={styles.hotBox}>
-          <Text>인기 급상승 영상</Text>
+      <View style={styles.topBox}>
+        <View style={styles.myVlogText}>
+          <Text style={styles.screenTitle}>나만의 Vlog</Text>
+          <Icon name='play-circle' type='font-awesome' size={24} color='#FE6788'></Icon>
         </View>
-        <View style={styles.hotBox}>
-          <Text>인기 급하락 영상</Text>
-        </View>
-      </ScrollView>
+        <TouchableOpacity>
+          <Icon name='search' type='feather' size={24} style={{justifyContent:'flex-end'}}></Icon>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.hotText}>이번주 Hot 랭킹을 확인해보세요!!</Text>
+      <View>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator = {false}
+          pagingEnabled
+          style={styles.horizontalScrollView}>
+          <View style={styles.hotBox}>
+            <Text style={styles.contentTitle}>인기 급상승 영상 Top 1</Text>
+            <Video
+              source={{ uri: videoUri }}
+              paused={true}
+              style={styles.video}
+              controls={true}
+              resizeMode={"cover"}
+              audioOnly={false} // true로 주면 비디오 로딩되어도 poster 안사라짐
+              poster={loadingUri} // 썸네일이 아니라 비디오 로딩 화면
+              posterResizeMode={"center"} 
+            />
+            <View style={styles.hotBottomText}>
+              <Text style={styles.hashTag}>{hashTags}</Text>
+              <View style={styles.heartBox}>
+              <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
+                <Text style={styles.heartText}>22</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.hotBox}>
+            <Text style={styles.contentTitle}>인기 급상승 영상 Top 2</Text>
+            <Video
+              source={{ uri: videoUri }}
+              paused={true}
+              style={styles.video}
+              controls={true}
+              resizeMode={"cover"}
+              audioOnly={false} // true로 주면 비디오 로딩되어도 poster 안사라짐
+              poster={loadingUri} // 썸네일이 아니라 비디오 로딩 화면
+              posterResizeMode={"center"} 
+            />
+            <View style={styles.hotBottomText}>
+              <Text style={styles.hashTag}>{hashTags}</Text>
+              <View style={styles.heartBox}>
+              <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
+                <Text style={styles.heartText}>22</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.hotBox}>
+            <Text style={styles.contentTitle}>인기 급상승 영상 Top 3</Text>
+            <Video
+              source={{ uri: videoUri }}
+              paused={true}
+              style={styles.video}
+              controls={true}
+              resizeMode={"cover"}
+              audioOnly={false} 
+              poster={loadingUri}
+              posterResizeMode={"center"} 
+            />
+            <View style={styles.hotBottomText}>
+              <Text style={styles.hashTag}>{hashTags}</Text>
+              <View style={styles.heartBox}>
+              <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
+                <Text style={styles.heartText}>22</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
       <View style={styles.scrollCircle}>
         <Icon name='circle' type="font-awesome" color='#384BF5' size={9} style={styles.circle} />
         <Icon name='circle' type="font-awesome" color='#D9D9D9' size={9} style={styles.circle} />
+        <Icon name='circle' type="font-awesome" color='#D9D9D9' size={9} style={styles.circle} />
       </View>
-      <ScrollView
-        showsVerticalScrollIndicator = {false}
-        style={styles.verticalScrollView}>
-        <View style={styles.center}>
-          <View style={styles.newBox}>
-            <Text>최신 영상</Text>
+      <View style={styles.verticalScrollView}>
+        <Text style={styles.newText}>최신 영상을 확인해보세요!</Text>
+        <ScrollView
+          showsVerticalScrollIndicator = {false}>
+          <View style={styles.center}>
+            <View style={styles.newBox}>
+              <Text style={styles.contentTitle}>최신 영상</Text>
+              <Video
+                source={{ uri: videoUri }}
+                paused={true}
+                style={styles.video}
+                controls={true}
+                resizeMode={"cover"}
+                audioOnly={false} 
+                poster={loadingUri}
+                posterResizeMode={"center"} 
+              />
+            </View>
           </View>
-          <View style={styles.newBox}>
-            <Text>덜 최신 영상</Text>
-          </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -45,22 +125,42 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     backgroundColor: '#FFFBFD',
-    alignItems: 'center'
+  },
+  myVlogText:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  topBox: {
+    margin: 10,
+    marginHorizontal: 30,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  screenTitle: {
+    fontSize: 22,
+    color: '#384BF5',
+    fontWeight: 'bold',
+    marginRight: 5
+  },
+  hotText: {
+    marginLeft: 30,
+    fontSize: 20,
   },
   hotBox: {
-    margin: 37.5,
-    width: 300,
-    height: 200,
+    marginHorizontal: wp('6%'),
+    marginVertical: 20,
     backgroundColor: '#F4F6F9',
     borderRadius: 10,
+    padding: 5,
     ...Platform.select({
       ios: {
         shadowColor: "#000",
         shadowOffset: {
-          width: 3,
-          height: 3,
+          width: 5,
+          height: 5,
         },
-        shadowOpacity: 0.1,
+        shadowOpacity: 0.3,
         shadowRadius: 5,
       },
       android: {
@@ -68,10 +168,23 @@ const styles = StyleSheet.create({
       },
     }),
   },
+  hotBottomText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 15,
+    paddingTop: 0,
+    alignItems: 'center'
+  },
+  heartBox: {
+    flexDirection: 'row'
+  },
+  heartText: {
+    color: '#FE646F',
+    marginLeft: 5
+  },
   scrollCircle: {
     flexDirection: 'row',
     justifyContent: 'center',
-    marginTop: 10,
     marginBottom: 15
   },
   circle: {
@@ -81,17 +194,39 @@ const styles = StyleSheet.create({
   verticalScrollView: {
     backgroundColor: '#E5F0FF',
     width: '100%',
+    height: hp('35%'),
   },
   newBox: {
-    width: 300,
-    height: 200,
     borderRadius: 10,
     backgroundColor: '#FFFBFD',
-    marginTop: 30,
+    marginBottom: 20,
+    padding: 5
   },
   center: {
     alignItems: 'center'
-  }
+  },
+  contentTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    margin: 10,
+    marginBottom: 0
+  },
+  newText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#3A4563',
+    margin: 20,
+    marginBottom: 15
+  },
+  video: {
+    width: 300,
+    height: 200,
+    backgroundColor: '#F1F4F9',
+    borderRadius: 10,
+    borderWidth: 1.5,
+    borderColor: '#F1F4F9',
+    margin: 10
+  },
 });
 
 export default FeedHome;
