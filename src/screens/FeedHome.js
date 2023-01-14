ff2dbd8b8726cbdc7c9747b087e10a77e12c467e
BBIYAC <kiy7833@gmail.com>
2023-01-14 11:34:08 +0900

    feat(FeedHome): 피드 목록 화면 UI 구현 완료

--- src/screens/FeedHome.js
+++ src/screens/FeedHome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
 import { Icon } from "@rneui/themed";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Video from "react-native-video";
@@ -10,8 +10,11 @@ const FeedHome = () => {
   const loadingUri = "/Users/in-yeong/iykim/videoDot/src/assets/videoLoadingGif.gif";
   const hashTags = '#V #목적없이떠나는여행 #꼬막비빔밥';
 
-  return (
-    <View style={styles.container}>
+  return (  
+    <ScrollView
+      showsVerticalScrollIndicator = {false}
+      bounces = {false}
+      style={styles.verticalScrollView}>
       <View style={styles.topBox}>
         <View style={styles.myVlogText}>
           <Text style={styles.screenTitle}>나만의 Vlog</Text>
@@ -22,54 +25,88 @@ const FeedHome = () => {
         </TouchableOpacity>
       </View>
       <Text style={styles.hotText}>이번주 Hot 랭킹을 확인해보세요!!</Text>
-      <View>
-        <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator = {false}
-          pagingEnabled
-          style={styles.horizontalScrollView}>
-          <View style={styles.hotBox}>
-            <Text style={styles.contentTitle}>인기 급상승 영상 Top 1</Text>
-            <Video
-              source={{ uri: videoUri }}
-              paused={true}
-              style={styles.video}
-              controls={true}
-              resizeMode={"cover"}
-              audioOnly={false} // true로 주면 비디오 로딩되어도 poster 안사라짐
-              poster={loadingUri} // 썸네일이 아니라 비디오 로딩 화면
-              posterResizeMode={"center"} 
-            />
-            <View style={styles.hotBottomText}>
-              <Text style={styles.hashTag}>{hashTags}</Text>
-              <View style={styles.heartBox}>
-              <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
-                <Text style={styles.heartText}>22</Text>
-              </View>
+      <ScrollView
+        horizontal={true}
+        showsHorizontalScrollIndicator = {false}
+        pagingEnabled
+        style={styles.horizontalScrollView}>
+        <View style={styles.hotBox}>
+          <Text style={styles.contentTitle}>인기 급상승 영상 Top 1</Text>
+          <Video
+            source={{ uri: videoUri }}
+            paused={true}
+            style={styles.video}
+            controls={true}
+            resizeMode={"cover"}
+            audioOnly={false} // true로 주면 비디오 로딩되어도 poster 안사라짐
+            poster={loadingUri} // 썸네일이 아니라 비디오 로딩 화면
+            posterResizeMode={"center"} 
+          />
+          <View style={styles.hotBottomText}>
+            <Text style={styles.hashTag}>{hashTags}</Text>
+            <View style={styles.heartBox}>
+            <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
+              <Text style={styles.heartText}>22</Text>
             </View>
           </View>
-          <View style={styles.hotBox}>
-            <Text style={styles.contentTitle}>인기 급상승 영상 Top 2</Text>
-            <Video
-              source={{ uri: videoUri }}
-              paused={true}
-              style={styles.video}
-              controls={true}
-              resizeMode={"cover"}
-              audioOnly={false} // true로 주면 비디오 로딩되어도 poster 안사라짐
-              poster={loadingUri} // 썸네일이 아니라 비디오 로딩 화면
-              posterResizeMode={"center"} 
-            />
-            <View style={styles.hotBottomText}>
-              <Text style={styles.hashTag}>{hashTags}</Text>
-              <View style={styles.heartBox}>
+        </View>
+        <View style={styles.hotBox}>
+          <Text style={styles.contentTitle}>인기 급상승 영상 Top 2</Text>
+          <Video
+            source={{ uri: videoUri }}
+            paused={true}
+            style={styles.video}
+            controls={true}
+            resizeMode={"cover"}
+            audioOnly={false} // true로 주면 비디오 로딩되어도 poster 안사라짐
+            poster={loadingUri} // 썸네일이 아니라 비디오 로딩 화면
+            posterResizeMode={"center"} 
+          />
+          <View style={styles.hotBottomText}>
+            <Text style={styles.hashTag}>{hashTags}</Text>
+            <View style={styles.heartBox}>
               <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
-                <Text style={styles.heartText}>22</Text>
-              </View>
+              <Text style={styles.heartText}>22</Text>
             </View>
           </View>
-          <View style={styles.hotBox}>
-            <Text style={styles.contentTitle}>인기 급상승 영상 Top 3</Text>
+        </View>
+        <View style={styles.hotBox}>
+          <Text style={styles.contentTitle}>인기 급상승 영상 Top 3</Text>
+          <Video
+            source={{ uri: videoUri }}
+            paused={true}
+            style={styles.video}
+            controls={true}
+            resizeMode={"cover"}
+            audioOnly={false} 
+            poster={loadingUri}
+            posterResizeMode={"center"} 
+          />
+          <View style={styles.hotBottomText}>
+            <Text style={styles.hashTag}>{hashTags}</Text>
+            <View style={styles.heartBox}>
+              <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
+              <Text style={styles.heartText}>22</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+      <View style={styles.scrollCircle}>
+        <Icon name='circle' type="font-awesome" color='#384BF5' size={9} style={styles.circle} />
+        <Icon name='circle' type="font-awesome" color='#D9D9D9' size={9} style={styles.circle} />
+        <Icon name='circle' type="font-awesome" color='#D9D9D9' size={9} style={styles.circle} />
+      </View>
+      <View style={styles.newContainer}>
+        <Text style={styles.newText}>최신 영상을 확인해보세요!</Text>
+        <View style={styles.center}>
+          <View style={styles.newBox}>
+            <View style={styles.userInfo}>
+              <Image source={require('../assets/userPhoto.png')} style={styles.userImage}></Image>
+              <View>
+                <Text style={styles.contentTitle}>해시태그닷</Text>
+                <Text style={styles.userUploadTime}>1분 전・을지로</Text>
+              </View>
+            </View>
             <Video
               source={{ uri: videoUri }}
               paused={true}
@@ -81,49 +118,103 @@ const FeedHome = () => {
               posterResizeMode={"center"} 
             />
             <View style={styles.hotBottomText}>
-              <Text style={styles.hashTag}>{hashTags}</Text>
+              <Text style={styles.newHashTag}>{hashTags}</Text>
               <View style={styles.heartBox}>
-              <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
+                <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
                 <Text style={styles.heartText}>22</Text>
               </View>
             </View>
+            <Text style={styles.videoTitle}>목적없이 떠나는 드라이브 VLOG</Text>
           </View>
-        </ScrollView>
-      </View>
-      <View style={styles.scrollCircle}>
-        <Icon name='circle' type="font-awesome" color='#384BF5' size={9} style={styles.circle} />
-        <Icon name='circle' type="font-awesome" color='#D9D9D9' size={9} style={styles.circle} />
-        <Icon name='circle' type="font-awesome" color='#D9D9D9' size={9} style={styles.circle} />
-      </View>
-      <View style={styles.verticalScrollView}>
-        <Text style={styles.newText}>최신 영상을 확인해보세요!</Text>
-        <ScrollView
-          showsVerticalScrollIndicator = {false}>
-          <View style={styles.center}>
-            <View style={styles.newBox}>
-              <Text style={styles.contentTitle}>최신 영상</Text>
-              <Video
-                source={{ uri: videoUri }}
-                paused={true}
-                style={styles.video}
-                controls={true}
-                resizeMode={"cover"}
-                audioOnly={false} 
-                poster={loadingUri}
-                posterResizeMode={"center"} 
-              />
+          <View style={styles.newBox}>
+            <View style={styles.userInfo}>
+              <Image source={require('../assets/userPhoto.png')} style={styles.userImage}></Image>
+              <View>
+                <Text style={styles.contentTitle}>해시태그닷</Text>
+                <Text style={styles.userUploadTime}>1분 전・을지로</Text>
+              </View>
             </View>
+            <Video
+              source={{ uri: videoUri }}
+              paused={true}
+              style={styles.video}
+              controls={true}
+              resizeMode={"cover"}
+              audioOnly={false} 
+              poster={loadingUri}
+              posterResizeMode={"center"} 
+            />
+            <View style={styles.hotBottomText}>
+              <Text style={styles.newHashTag}>{hashTags}</Text>
+              <View style={styles.heartBox}>
+                <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
+                <Text style={styles.heartText}>22</Text>
+              </View>
+            </View>
+            <Text style={styles.videoTitle}>목적없이 떠나는 드라이브 VLOG</Text>
           </View>
-        </ScrollView>
+          <View style={styles.newBox}>
+            <View style={styles.userInfo}>
+              <Image source={require('../assets/userPhoto.png')} style={styles.userImage}></Image>
+              <View>
+                <Text style={styles.contentTitle}>해시태그닷</Text>
+                <Text style={styles.userUploadTime}>1분 전・을지로</Text>
+              </View>
+            </View>
+            <Video
+              source={{ uri: videoUri }}
+              paused={true}
+              style={styles.video}
+              controls={true}
+              resizeMode={"cover"}
+              audioOnly={false} 
+              poster={loadingUri}
+              posterResizeMode={"center"} 
+            />
+            <View style={styles.hotBottomText}>
+              <Text style={styles.newHashTag}>{hashTags}</Text>
+              <View style={styles.heartBox}>
+                <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
+                <Text style={styles.heartText}>22</Text>
+              </View>
+            </View>
+            <Text style={styles.videoTitle}>목적없이 떠나는 드라이브 VLOG</Text>
+          </View>
+          <View style={styles.newBox}>
+            <View style={styles.userInfo}>
+              <Image source={require('../assets/userPhoto.png')} style={styles.userImage}></Image>
+              <View>
+                <Text style={styles.contentTitle}>해시태그닷</Text>
+                <Text style={styles.userUploadTime}>1분 전・을지로</Text>
+              </View>
+            </View>
+            <Video
+              source={{ uri: videoUri }}
+              paused={true}
+              style={styles.video}
+              controls={true}
+              resizeMode={"cover"}
+              audioOnly={false} 
+              poster={loadingUri}
+              posterResizeMode={"center"} 
+            />
+            <View style={styles.hotBottomText}>
+              <Text style={styles.newHashTag}>{hashTags}</Text>
+              <View style={styles.heartBox}>
+                <Icon name='heart' type='font-awesome' color='#FE646F' size={16}></Icon>
+                <Text style={styles.heartText}>22</Text>
+              </View>
+            </View>
+            <Text style={styles.videoTitle}>목적없이 떠나는 드라이브 VLOG</Text>
+          </View>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: '100%',
+  verticalScrollView: {
     backgroundColor: '#FFFBFD',
   },
   myVlogText:{
@@ -153,12 +244,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#F4F6F9',
     borderRadius: 10,
     padding: 5,
+    paddingBottom: 10,
     ...Platform.select({
       ios: {
         shadowColor: "#000",
         shadowOffset: {
-          width: 5,
-          height: 5,
+          width: 2,
+          height: 2,
         },
         shadowOpacity: 0.3,
         shadowRadius: 5,
@@ -171,8 +263,8 @@ const styles = StyleSheet.create({
   hotBottomText: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: 15,
-    paddingTop: 0,
+    paddingHorizontal: 15,
+    paddingBottom: 5,
     alignItems: 'center'
   },
   heartBox: {
@@ -191,16 +283,22 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5
   },
-  verticalScrollView: {
+  newContainer: {
     backgroundColor: '#E5F0FF',
-    width: '100%',
-    height: hp('35%'),
+    paddingBottom: 30,
   },
   newBox: {
     borderRadius: 10,
     backgroundColor: '#FFFBFD',
     marginBottom: 20,
     padding: 5
+  },
+  newText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#3A4563',
+    margin: 25,
+    marginBottom: 15
   },
   center: {
     alignItems: 'center'
@@ -211,12 +309,21 @@ const styles = StyleSheet.create({
     margin: 10,
     marginBottom: 0
   },
-  newText: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    color: '#3A4563',
-    margin: 20,
-    marginBottom: 15
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 5,
+    marginBottom: 0
+  },
+  userImage: {
+    width: 50,
+    height: 50
+  },
+  userUploadTime: {
+    color: 'gray',
+    marginLeft: 10,
+    marginTop: 3,
+    fontSize: 12
   },
   video: {
     width: 300,
@@ -227,6 +334,16 @@ const styles = StyleSheet.create({
     borderColor: '#F1F4F9',
     margin: 10
   },
+  newHashTag: {
+    color: '#384BF5',
+    fontWeight: 'bold'
+  },
+  videoTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginHorizontal: 15,
+    marginBottom: 10
+  }
 });
 
 export default FeedHome;
