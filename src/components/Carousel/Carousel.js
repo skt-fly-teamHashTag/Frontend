bdd07547571180dab48c338a0f503d4dff2b004b
BBIYAC <kiy7833@gmail.com>
2023-02-02 20:24:45 +0900

    fix(Carousel): 피드 자동 넘김 제거

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -6,58 +6,30 @@ import CarouselItem from './CarouselItem'
 const { width, heigth } = Dimensions.get('window')
 let flatList
 
-function infiniteScroll(dataList){
-    const numberOfData = dataList.length
-    let scrollValue = 0, scrolled = 0
-
-    setInterval(() => {
-        scrolled ++
-        if(scrolled < numberOfData)
-        scrollValue = scrollValue + width
-
-        else{
-            scrollValue = 0
-            scrolled = 0
-        }
-        
-        if(this.flatList != null){
-          this.flatList.scrollToOffset({animated: true, offset: scrollValue})
-         }
-        
-    }, 3000)
-}
-
-
 const Carousel = ({ data, showToast }) => {
     const scrollX = new Animated.Value(0)
     let position = Animated.divide(scrollX, width)
     const [dataList, setDataList] = useState(data)
-
-    useEffect(()=> {
-      setDataList(data)
-      infiniteScroll(dataList)
-    }, []);
-
 
     if (data && data.length) {
       return (
         <View>
           <FlatList data={data}
             ref = {(flatList) => {this.flatList = flatList}}
-              keyExtractor={(item, index) => 'key' + index}
-              horizontal
-              pagingEnabled
-              scrollEnabled
-              snapToAlignment="center"
-              decelerationRate={"fast"}
-              showsHorizontalScrollIndicator={false}
-              renderItem={({ item, index }) => {
-                  return <CarouselItem item={item} index={index} showToast={showToast} />
-              }}
-              onScroll={Animated.event(
-                  [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-                  { useNativeDriver: false }, 
-              )}
+            keyExtractor={(item, index) => 'key' + index}
+            horizontal
+            pagingEnabled
+            scrollEnabled
+            snapToAlignment="center"
+            decelerationRate={"fast"}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item, index }) => {
+                return <CarouselItem item={item} index={index} showToast={showToast} />
+            }}
+            onScroll={Animated.event(
+                [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                { useNativeDriver: false }, 
+            )}
           />
 
           <View style={styles.dotView}>
