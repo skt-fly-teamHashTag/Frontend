8d6aa68ebf7a45941f99177a348f7025863821cb
BBIYAC <kiy7833@gmail.com>
2023-02-06 21:42:46 +0900

    fix(Search): 업로드 날짜 수정

--- src/components/Search/SearchItem.js
+++ src/components/Search/SearchItem.js
@@ -45,13 +45,31 @@ const SearchItem = ({ item, showToast }) => {
     showToast(isLiked);
   };
 
+  const getUploadedAt = () => {
+    const milliSeconds = item.uploadedAt;
+    const seconds = milliSeconds / 1000;
+    if (seconds < 60) return `방금 전`;
+    const minutes = seconds / 60;
+    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+    const hours = minutes / 60;
+    if (hours < 24) return `${Math.floor(hours)}시간 전`;
+    const days = hours / 24;
+    if (days < 7) return `${Math.floor(days)}일 전`;
+    const weeks = days / 7;
+    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
+    const months = days / 30;
+    if (months < 12) return `${Math.floor(months)}개월 전`;
+    const years = days / 365;
+    return `${Math.floor(years)}년 전`;
+  }
+
   return (
     <View style={styles.newBox}>
       <View style={styles.userInfo}>
         <Image source={require('../../assets/userPhoto.png')} style={styles.userImage}></Image>
         <View>
           <Text style={styles.contentTitle}>{ item.nickName }</Text>
-          <Text style={styles.userUploadTime}>{ item.uploadTime }</Text>
+          <Text style={styles.userUploadTime}>{ getUploadedAt() }</Text>
         </View>
       </View>
       <TouchableOpacity activeOpacity={0.8} onPress={onPressVideo}>
