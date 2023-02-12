7aa3cfe2fd8dc74c72e6e40b3210ebf0d39e1c20
BBIYAC <kiy7833@gmail.com>
2023-02-12 23:15:27 +0900

    style(VideoFullscreen): 전체화면시 상태바 색상 수정

--- src/screens/VideoFullscreen.js
+++ src/screens/VideoFullscreen.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { View } from "react-native"
 import VideoPlayer from "react-native-video-controls";
+import { StatusBar } from "react-native";
 
 const VideoFullscreen = ({ navigation, route }) => {
   const videoPath = route.params;
   return (
     <View style={{ width:'100%', height: '100%' }}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor={'transparent'} 
+        translucent={true} /> 
       <VideoPlayer
         source={{ uri: videoPath }}
         onBack={navigation.goBack}
